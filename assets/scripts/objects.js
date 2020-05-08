4cbef6b36a0bf6d031d6105d7f15146bfb8550bb
Hadi Arbabi <h.arbabi@banicm.com>
2020-05-08 15:14:55 +0430

    last commit

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,76 +1,35 @@
-const addMovieButton = document.getElementById("add-movie-btn"); //button
-const searchMovieButton = document.getElementById("search-btn");
-//  ---------------------------------------------
+'use strict';
+const addMovieBtn = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('search-btn');
+
 const movies = [];
 
-const addMovieHandler = () => {
-  const movieTitle = document.getElementById("title").value;
-  const extraName = document.getElementById("extra-name").value;
-  const extraValue = document.getElementById("extra-value").value;
-  if (
-    movieTitle.trim() === "" ||
-    extraName.trim() === "" ||
-    extraValue.trim() === ""
-  ) {
-    // console.log("bad input");
+const renderMovies = (filter = '') => {
+  const movieList = document.getElementById('movie-list');
+
+  if (movies.length === 0) {
+    movieList.classList.remove('visible');
     return;
-  }
-
-  const newMovie = {
-    info: {
-      title: movieTitle, //if movieTitle name is title we can use title keyname and value name are the same
-      [extraName]: extraValue,
-    },
-    id: Math.random(),
-    getFormatedTitle() {
-      console.log(this);
-      return this.info.title.toUpperCase();
-    },
-  };
-  movies.push(newMovie);
-  // console.log(movies);
-  // console.log("here");
-  // renderMovie(newMovie.info.title, extraValue, extraName, newMovie.id); //render movie with LIST ITEMS
-  renderMovie();
-};
-
-// const renderMovie = (title, extraName, extraValue, id) => {
-//   // this is with list items
-//   const movieList = document.getElementById("movie-list"); //unordered list
-//   const li = document.createElement("li");
-//   li.innerHTML = `<div> <pre>${title}   ${extraValue}  ${extraName}    with  id : ${id}</pre></div>`;
-//   movieList.append(li);
-//   movieList.classList.add("visible");
-// };
-const renderMovie = (filter = "") => {
-  const movieList = document.getElementById("movie-list"); //unordered list
-  if (movies.length !== 0) {
-    movieList.classList.add("visible");
   } else {
-    movieList.classList.remove("visible");
-    return;
+    movieList.classList.add('visible');
   }
-  movieList.innerHTML = "";
+  movieList.innerHTML = '';
 
-  const filteredMovie = !filter
+  const filteredMovies = !filter
     ? movies
-    : movies.filter((movie) => movie.info.title.includes(filter)); //search process
+    : movies.filter(movie => movie.info.title.includes(filter));
 
-  filteredMovie.forEach((movie) => {
-    const movieEl = document.createElement("li");
-    // if ("info" in movie) {
-    //   console.log("info is property on object movie");
-    // }
-
-    const { info, ...otherprops } = movie;
-    // console.log(otherprops, "--------------");
+  filteredMovies.forEach(movie => {
+    const movieEl = document.createElement('li');
+    const { info, ...otherProps } = movie;
+    console.log(otherProps);
     // const { title: movieTitle } = info;
-    let { getFormatedTitle } = movie;
-    // getFormatedTitle = getFormatedTitle.bind(movie);
-    let text = getFormatedTitle.call(movie) + "_";
+    let { getFormattedTitle } = movie;
+    // getFormattedTitle = getFormattedTitle.bind(movie);
+    let text = getFormattedTitle.apply(movie) + ' - ';
     for (const key in info) {
-      if (key !== "title") {
-        text = text + `${key}:${info[key]} `;
+      if (key !== 'title' && key !== '_title') {
+        text = text + `${key}: ${info[key]}`;
       }
     }
     movieEl.textContent = text;
@@ -78,12 +37,51 @@ const renderMovie = (filter = "") => {
   });
 };
 
-const searchMovieHandler = () => {
-  // const searchMovieHandler = function () {
-  console.log(this);
-  const filterTitle = document.getElementById("filter-title").value;
-  renderMovie(filterTitle);
+const addMovieHandler = () => {
+  const title = document.getElementById('title').value;
+  const extraName = document.getElementById('extra-name').value;
+  const extraValue = document.getElementById('extra-value').value;
+
+  if (
+    extraName.trim() === '' ||
+    extraValue.trim() === ''
+  ) {
+    return;
+  }
+
+  const newMovie = {
+    info: {
+      set title(val) {
+        if (val.trim() === '') {
+          this._title = 'DEFAULT';
+          return;
+        }
+        this._title = val;
+      },
+      get title() {
+        return this._title;
+      },
+      [extraName]: extraValue
+    },
+    id: Math.random().toString(),
+    getFormattedTitle() {
+      console.log(this);
+      return this.info.title.toUpperCase();
+    }
+  };
+
+  newMovie.info.title = title;
+  console.log(newMovie.info.title);
+
+  movies.push(newMovie);
+  renderMovies();
 };
 
-addMovieButton.addEventListener("click", addMovieHandler);
-searchMovieButton.addEventListener("click", searchMovieHandler);
+const searchMovieHandler = () => {
+  console.log(this);
+  const filterTerm = document.getElementById('filter-title').value;
+  renderMovies(filterTerm);
+};
+
+addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMovieHandler);
